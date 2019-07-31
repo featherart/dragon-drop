4b568940c123998b479301eca73d396102a53c31
Feather Knee <fknee@nvidia.com>
2019-07-31 16:01:20 -0700

    make it a separate component and dragon flies while it is being dragged yay!

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { DragonDrop } from './DragonDrop'
 
 function App() {
-  const [isDragging, setDragging] = useState(false)
-
-  const allowDrop = e => {
-    console.log('in allowDrop')
-    setDragging(true)
-    e.preventDefault()
-  }
-
-  const drag = e => {
-    console.log('in here', e)
-    return e.dataTransfer.setData("text", e.target.id)
-  }
-
-  const drop = e => {
-    e.preventDefault()
-    setDragging(false)
-    var data = e.dataTransfer.getData("text")
-    e.target.appendChild(document.getElementById(data))
-  }
-
 	return (
 		<div className='app-container'>
 			<h1>Dragon Drop</h1>
-      <div className='container'>
-        {isDragging && <img src='dragon-animated1.gif' />}
-        <img id='drag1' src='dragon.png' draggable='true' onDragStart={e => drag(e)} />
-        <div id='div1' className='drop-box' onDrop={e => drop(e)} onDragOver={e => allowDrop(e)}></div>
-      </div>
+      <DragonDrop />
 		</div>
 	)
 }
