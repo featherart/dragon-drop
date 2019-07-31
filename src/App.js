92137b5bb75dd37f4852583806063c899dbec569
Feather Knee <fknee@nvidia.com>
2019-07-31 14:04:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	return (
+		<div className='App'>
+			<h1>Dragon Drop</h1>
+      <div className='container'>
+        <img id='drag1' src='dragon-animated1.gif' draggable='true' />
+        <div id='div1' className='drop-box'></div>
+      </div>
+		</div>
+	)
 }
 
-export default App;
+export default App
